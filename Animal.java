96353239d7af016c6249b2b11639de12cdbdb1c3
Ken <ken.liu1@ucalgary.ca>
2023-03-29 15:20:01 -0600

    Created Animal.java and added code
    - Added data members in Animal.java
    - Added Animal Constructor
    - Added  getters

---
+++ Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String nickname;
+    private final int ANIMALID;
+    private final String ANIMALSPECIES;
+
+    public Animal(String nickname, int id, Strng species) {
+        this.nickname = nickname;
+        this.ANIMALID = id;
+        this.ANIMALSPECIES = speices;
+    }
+
+    public String getNickname() { return this.nickname; }
+    public int getAnimalID() { return this.ANIMALID;}
+    public String getSpecies() {return this.ANIMALSPECIES; }
+}
