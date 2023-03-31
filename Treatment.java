d5975e3d68045d5cc1ba40d6f833bde923de016b
rutvi01 <113965086+rutvi01@users.noreply.github.com>
2023-03-31 10:54:55 -0600

    Code Added to Treatment.java file

--- Treatment.java
+++ Treatment.java
@@ -1,1 +1,17 @@
 //Treatment
+public class Treatment {
+    private final int TREATMENTID;
+    private final int animalID;
+
+    /*
+     Constructor to set treatmentId, id to their respective data fields.
+    */
+    public Treatment(int treatmentID, int id) {
+        this.TREATMENTID = treatmentid;
+        this.animalID = id;   
+    }
+
+    // Getters
+    public String getTreatmentID() { return this.TREATMENTID; }
+    public int getAnimalID() { return this.animalID;}
+}
