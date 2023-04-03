6d0cecbc0d83f33f3f26f49422e4fd7a25d24e23
Ken <ken.liu1@ucalgary.ca>
2023-04-03 14:35:28 -0600

    Fixed Hour.java constructor to fit Schedule.java

--- edu/ucalgary/oop/Hour.java
+++ edu/ucalgary/oop/Hour.java
@@ -1,5 +1,7 @@
 package edu.ucalgary.oop;
 
+import java.util.ArrayList;
+
 public class Hour {
     private int timeAvailable = 60;
     private final int HOUR;
@@ -11,18 +13,17 @@ public class Hour {
      * also, takes in an ArrayList<Task> and setting it to tasks
      * Throws an exception if the hour is not between 0 and 23
      */
-    public Hour(int hour, ArrayList<Task> tasks) throws IllegalArgumentException{
+    public Hour(int hour) throws IllegalArgumentException{
         if (hour < 0 || hour > 23) {
             throw new IllegalArgumentException();
         }
         this.HOUR = hour;
-        this.tasks = tasks;
     }
 
     // Getters and setters
     public int getHour() { return this.HOUR; } 
     public int getTimeAvailable() {return this.timeAvailable; }
-    public Task getTasks() {return this.tasks; }
+    public ArrayList<Task> getTasks() {return this.tasks; }
     public boolean getBackupBoolean() { return this.backupBoolean; }
     public void setBackupBoolean(boolean backupBoolean) { this.backupBoolean = backupBoolean; }
 
