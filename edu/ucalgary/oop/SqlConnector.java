b218cfc0d831ac4e2e6fa3eaf625685441f0f42a
HylianMuffins <catholiczach@gmail.com>
2023-04-03 21:56:26 -0600

    Fully working sql connection

--- edu/ucalgary/oop/SqlConnector.java
+++ edu/ucalgary/oop/SqlConnector.java
@@ -1,18 +1,51 @@
 package edu.ucalgary.oop;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class SqlConnector {
-  public static void main(String args[]){  
-    try{  
-    Class.forName("com.mysql.cj.jdbc.Driver");  
-    Connection con=DriverManager.getConnection("jdbc:mysql://localhost/EWR","oop","password");  
-    //here sonoo is database name, root is username and password  
-    Statement stmt=con.createStatement();
-    ResultSet rs=stmt.executeQuery("select * from animals");  
-    while(rs.next())  
-    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-    con.close();  
-    }catch(Exception e){ System.out.println(e);}  
-    }  
+  private ArrayList<Animal> animals = new ArrayList<Animal>();
+  private ArrayList<Task> treatments = new ArrayList<Task>();
+
+  public SqlConnector() {
+    try{
+      // create connection with oop user
+      Class.forName("com.mysql.cj.jdbc.Driver");  
+      Connection con=DriverManager.getConnection("jdbc:mysql://localhost/EWR","oop","password");
+
+      // write and execute a query that gets all the animal info 
+      Statement stmt=con.createStatement();
+      ResultSet animalResults = stmt.executeQuery("SELECT * FROM animals");
+
+      // add an animal to the arraylist for each result
+      while(animalResults.next()) {
+        this.animals.add( new Animal(animalResults.getString(2), 
+          animalResults.getInt(1), animalResults.getString(3)));
+      }
+
+      // write a query that joins the correct task info to each treatment in treatments table
+      ResultSet treatmentResults = stmt.executeQuery("SELECT * FROM treatments INNER JOIN tasks ON tasks.TaskID=treatments.TaskID;");
+
+      // add a treatment to the arraylist for each result
+      while(treatmentResults.next()) {
+        this.treatments.add( new Treatment(treatmentResults.getString(6), treatmentResults.getInt(7), 
+          treatmentResults.getInt(8), treatmentResults.getInt(4), treatmentResults.getInt(1),
+          treatmentResults.getInt(2)));
+      }
+
+      // close connection
+      con.close();
+      
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public ArrayList<Animal> getAnimals() {
+    return this.animals;
+  }
+
+  public ArrayList<Task> getTreatments() {
+    return this.treatments;
+  }
 }
