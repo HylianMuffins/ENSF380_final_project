53051dde7aa12b13b5be4073a62a0cf1d2df65fa
Ken <ken.liu1@ucalgary.ca>
2023-04-04 16:37:11 -0600

    Fixed small typo for Raccoon enum (Racoon -> Racccoon)

--- edu/ucalgary/oop/AnimalTypes.java
+++ edu/ucalgary/oop/AnimalTypes.java
@@ -28,7 +28,7 @@ public enum AnimalTypes{
         }
     },
 
-    RACOON{
+    RACCOON{
         public int[] getTime() {
             int[] returnArray = {0, 5, 5, 0};
             return returnArray;
