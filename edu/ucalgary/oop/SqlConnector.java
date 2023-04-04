3bc5afd0825f98c7e755a3feb5b41e71b3dd0d8f
HylianMuffins <catholiczach@gmail.com>
2023-04-03 21:08:23 -0600

    added a conection to access data in sql table

---
+++ edu/ucalgary/oop/SqlConnector.java
@@ -0,0 +1,18 @@
+package edu.ucalgary.oop;
+
+import java.sql.*;
+
+public class SqlConnector {
+  public static void main(String args[]){  
+    try{  
+    Class.forName("com.mysql.cj.jdbc.Driver");  
+    Connection con=DriverManager.getConnection("jdbc:mysql://localhost/EWR","oop","password");  
+    //here sonoo is database name, root is username and password  
+    Statement stmt=con.createStatement();
+    ResultSet rs=stmt.executeQuery("select * from animals");  
+    while(rs.next())  
+    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+    con.close();  
+    }catch(Exception e){ System.out.println(e);}  
+    }  
+}
