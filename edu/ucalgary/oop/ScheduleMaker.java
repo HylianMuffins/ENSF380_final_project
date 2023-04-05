0fc11e33d79d4d3dbcb04fe1827f030faa1c9ac2
Ken <ken.liu1@ucalgary.ca>
2023-04-05 12:42:16 -0600

    Created methods to create and format the string to write into the text file, as well as the method to create the text file (only works for if no backups are needed for now)

--- edu/ucalgary/oop/ScheduleMaker.java
+++ edu/ucalgary/oop/ScheduleMaker.java
@@ -5,10 +5,10 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.EventQueue;
 import javax.swing.*;
-
 import java.awt.event.*;
 import java.util.*;
 import java.awt.GridBagLayout;
+import java.io.*;
 
 public class ScheduleMaker implements ActionListener, MouseListener{
     private SqlConnector sqlData = new SqlConnector();
@@ -66,23 +66,25 @@ public class ScheduleMaker implements ActionListener, MouseListener{
             }
 
             if (confirmBackupMessage.size() != 0) {
-                // TODO: change this to a specific exception later
-                throw new Exception();
+                throw new UnconfirmedBackupsException();
             }
 
-            createScheduleString(schedule);
+            createTextFile(createScheduleString(schedule));
+            
             JOptionPane.showMessageDialog(null, "Schedule has been successfully made!");
-    	} catch (Exception e) {
+    	} catch (UnconfirmedBackupsException e) {
     		JOptionPane.showMessageDialog(null, "Schedule is in need of backup, please confirm with the backups");
     	} catch (TimeConflictException e) {
             
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Something Unexepected has occured...");
+            //e.printStackTrace();
         }
 
     }
     
     private String createScheduleString(Schedule schedule) {
         this.scheduleString += "Schedule for " + schedule.getDate().toString() + "\n\n";
-
         // Loops through each hour, and if the time available has not changed then don't
         // store it into the string, otherwise, store the necessary information.
         schedule.getHourList().forEach(hour -> {
@@ -92,13 +94,36 @@ public class ScheduleMaker implements ActionListener, MouseListener{
                 // Loops through all tasks that are needed in that hour, and store its description
                 // into the string
                 for (int i = 0; i < hour.getTasks().size(); i++) {
-                    scheduleString += "* " + hour.getTasks().get(i).getDescription(); 
+                    scheduleString += "* " + hour.getTasks().get(i).getDescription() + "\n"; 
                 }
 
+                scheduleString += "\n";
             }
         });
         
         return scheduleString;
+    }
+
+    private void createTextFile(String schedule) {
+        try {
+            File textFile = new File("Schedule.txt");
+            
+            // Creates the file if it doesnt exist, else, replace the file
+            // and create the new file.
+            if(!textFile.exists()) {
+                textFile.createNewFile();
+            } else {
+                textFile.delete();
+                textFile.createNewFile();
+            }
+
+            // Create the FileWriter object to write into a text file
+            FileWriter textWriter = new FileWriter(textFile);
+            textWriter.write(schedule);
+            textWriter.close();
+        } catch (IOException e) {
+
+        }
     }
 
     public void mouseClicked(MouseEvent event){
